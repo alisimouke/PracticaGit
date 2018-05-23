0046f749f9b139864d700509d0acc95668d79687
Ali <alisoultana@hotmail.com>
2018-05-23 13:17:35 +0200

    comr

--- src/Clase1.java
+++ src/Clase1.java
@@ -4,4 +4,23 @@ public class Clase1 {
 int variable1;
 int variable2;
 
+
+
+
+
+public int getVariable1() {
+	return variable1;
+}
+public int getVariable2() {
+	return variable2;
+}
+public void setVariable1(int variable1) {
+	this.variable1 = variable1;
+}
+public void setVariable2(int variable2) {
+	this.variable2 = variable2;
+}
+
+
+
 }
